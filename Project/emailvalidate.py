0ecc958ef9b1bfe28763bd0244f84caab32888fe
Ahir Shiv <imahirshiv@gmail.com>
2022-09-28 17:53:46 +0530

    Newcommit

---
+++ Project/emailvalidate.py
@@ -0,0 +1,31 @@
+from curses.ascii import isdigit, isupper
+d,k,j=0,0,0
+email=input("Enter Your Email :-")
+if len(email)>=6:
+    if email[0].isalpha():
+        if ('@' in email) and (email.count("@")==1):
+            if (email[-4]==".") ^ (email[-3]=="."):
+                for i in email:
+                    if i==i.isspace():
+                        k=1
+                    elif i.isalpha():
+                        if i==i.upper():
+                            j=1
+                    elif i.isdigit():
+                        continue
+                    elif i=="." or i=="_" or i=="@":
+                        continue
+                    else:
+                        d=1
+                if k==1 or j==1 or d==1:
+                    print("Wrong email 5")
+                else:
+                    print("Right Email")
+            else:
+                print("Wrong email 4")
+        else:
+            print("Wrong email 3")
+    else:
+        print("Wrong email 2")
+else:
+    print("wrong email 1")
